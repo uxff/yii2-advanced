a688afd07aa13800ccc5da0fd25ff2c398b57d0b
uxff <uxff@qq.com>
2016-05-05 19:18:35 +0800

    优化lifegame

---
+++ frontend/views/lifegame2/showmap.php
@@ -0,0 +1,43 @@
+<div style="position:absolute">
+<i class="fa fa-cubes"></i>
+</div>
+<a href="index.php?r=lifegame2/makelife&ver=<?php echo $ver;?>&px=<?php echo $px;?>">生存</a>&nbsp;
+<a href="index.php?r=lifegame2/showmap&ver=<?php echo $pre;?>&px=<?php echo $px;?>">撤销</a>&nbsp;
+&nbsp;&nbsp;
+<a href="index.php?r=lifegame2/showmap&ver=<?php echo $ver;?>&px=<?php echo (int)($px+1);?>">放大 +</a>&nbsp;
+<a href="index.php?r=lifegame2/showmap&ver=<?php echo $ver;?>&px=<?php echo (int)($px-1);?>">缩小 -</a>&nbsp;
+&nbsp;&nbsp;
+边长:<input type="text" name="width" id="new_width" value="<?php echo $width;?>" style="width:40px"/>
+<a href="javascript:;" id="btn_new" onclick="gotonew()">新建图像</a>&nbsp;
+<a href="index.php?r=lifegame2/clearmap&ver=<?php echo $ver;?>">清除图像</a>&nbsp;
+<!--
+<a href="index.php?r=lifegame2/clearall">clearall</a>&nbsp;
+-->
+<pre>
+<?php
+if ($showtd) {
+    echo $mapStr;
+} else {
+?>
+    <img src="index.php?r=lifegame2/gdmap&ver=<?php echo $ver;?>&px=<?php echo $px;?>"/>
+<?php
+}
+
+?>
+
+</pre>
+<span>width=<?php echo $width;?></span>
+<span>height=<?php echo $height;?></span>
+<!--
+<span>deep=<?php echo $deep;?></span>
+-->
+<span>i=<?php echo $i;?></span>
+<span>dur=<?php echo $dur;?></span>
+<span>count=<?php echo $count;?></span>
+<span>maxLived=<?php echo $maxLived;?></span>
+<span>maxLivedDur=<?php echo $maxLivedDur;?></span>
+<script>
+var gotonew = function () {
+    window.location = 'index.php?r=lifegame2/makenewmap'+'&width='+document.getElementById('new_width').value;
+}
+</script>
