d814853526be630de6b7dd8b57f5072bd75b64b9
uxff <uxff@qq.com>
2018-01-16 19:04:35 +0800

    fix logo string

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'A dot publicserver MapTopo',
+        'brandLabel' => 'A dot MapTopo',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -89,7 +89,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; A dot publicserver <?= date('Y') ?></p>
+        <p class="pull-left">&copy; A dot maptopo <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
